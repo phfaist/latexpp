005dd9906895716bd1c08f443da2109611b0c147
Philippe Faist <phfaist@gmail.com>
2019-07-25 17:26:01 +0200

    starts being usable!!

---
+++ latexpp/fixes/add_preamble.py
@@ -0,0 +1,11 @@
+
+
+class Fixes(object):
+    def __init__(self, preamble):
+        self.preamble = preamble
+
+    def add_preamble(self):
+        return self.preamble
+
+    def fix_node(self, n, lpp):
+        return None
