b73d4a16cdbc51dc28b162ac84d6e0b077e556fc
Philippe Faist <phfaist@gmail.com>
2019-07-26 13:34:29 +0200

    a few command-line options, updated README

--- README.rst
+++ README.rst
@@ -21,14 +21,17 @@ Sample ``lppconfig.yml``:
 .. code-block:: yaml
 
   # latexpp config for MyDocument.tex
-  
-  # the master LaTeX document -- nothing will be modified, all output will be
-  # done in the output_dir
+  #
+  # This is YAML syntax -- google "YAML tutorial" to get a quick intro.  Careful
+  # with spaces, correct indentation is important.
+
+  # the master LaTeX document -- this file will not be modified, all output will
+  # be produced in the output_dir
   fname: 'MyDocument.tex'
 
   # output file(s) will be created in this directory, originals will not be
   # modified
-  output_dir: latexpp_output
+  output_dir: 'latexpp_output'
   
   # specify list of fixes to apply, in the given order
   fixes:
@@ -72,7 +75,7 @@ Sample ``lppconfig.yml``:
           # \braket{\psi}{\phi}  -->  \langle{\psi}\vert{\phi}\rangle
           braket:
             argspec: '{{'
-            repl: \langle{%(1)s}\vert{%(2)s}\rangle
+            repl: '\langle{%(1)s}\vert{%(2)s}\rangle'
 
 The config file follows standard YAML syntax (if you're in doubt, google a YAML
 tutorial).
@@ -98,6 +101,34 @@ Put you fix class in a module and set up your ``$PYTHONPATH`` so that the
 package/module is exposed to python.  Then simply specify the pacakge/module
 your fix is located in in the YAML file, instead of
 ``latexpp.fixes.xxxxx.XXXFixes``.
+
+
+How it actually works
+---------------------
+
+The ``latexpp`` preprocessor relies on `pylatexenc 2.0
+<https://github.com/phfaist/pylatexenc>`_ to parse the latex document into an
+internal node structure.  For instance, the chunk of latex code::
+  
+  Hello, \textit{world}! % show a greeting
+
+will be parsed into a list of four nodes, a ‘normal characters node’ ``"Hello,
+"``, a ‘macro node’ ``\textit`` with argument a ‘group node’ ``{world}`` which
+itself contains a ‘normal characters node’ ``world``, a ‘normal characters node’
+``"! "``, and a ‘latex comment node’ ``% show a greeting``.  The structure is
+recursive, with e.g. macro arguments and environment contents themselves
+represented as nodes which can contain further macros and environments.  See
+`pylatexenc.latexwalker
+<https://pylatexenc.readthedocs.io/en/latest/latexwalker/>`_ for more
+information.  The `pylatexenc` library has a list of some known macros and
+environments, and knows how to parse their arguments.  Some fixes in `latexpp`
+add their own macro and environment definitions.
+
+Once the latex document is parsed into the node structure, then the nodes are
+traversed recursively including macro arguments and environment contents.  For
+each node, we query all the fixes in the specified order to see if that return a
+latex representation of the given node.  If no fix is found, then the original
+latex representation of the node is retained.
 
 
 License
--- latexpp/__main__.py
+++ latexpp/__main__.py
@@ -3,28 +3,118 @@ import os.path
 import re
 import sys
 import importlib
+import argparse
 import logging
 logger = logging.getLogger(__name__)
 
 import yaml
 
+from . import __version__ as version_str
+
+
+_LPPCONFIG_DOC_URL = 'https://github.com/phfaist/latexpp/blob/master/README.rst'
+_LATEXPP_QUICKSTART_DOC_URL = 'https://github.com/phfaist/latexpp/blob/master/README.rst'
+
 
 from .preprocessor import LatexPreprocessor
+
+
+_lppconfig_template = r"""
+# latexpp config file template.
+#
+# This is YAML syntax -- google "YAML tutorial" to get a quick intro.  Careful
+# with spaces, correct indentation is important.
+
+# the master LaTeX document -- this file will not be modified, all output will
+# be produced in the output_dir
+fname: 'MyDocument.tex'
+
+# output file(s) will be created in this directory, originals will not be
+# modified
+output_dir: 'latexpp_output'
+
+# specify list of fixes to apply, in the given order
+fixes:
+
+  # remove all comments
+  - 'latexpp.fixes.comments.RemoveCommentsFixes'
+
+  # replace \input{...} directives by the contents of the included file
+  - 'latexpp.fixes.input.EvalInputFixes'
+
+  # copy any style files (.sty) that are used in the document and that
+  # are present in the current directory to the output directory
+  - 'latexpp.fixes.usepackage.CopyLocalPkgsFixes'
+
+  # copy figure files to the output directory and rename them fig-1.xxx,
+  # fig-2.xxx, etc.
+  - 'latexpp.fixes.figures.CopyNumberFigsFixes'
+
+  # Replace \bibliography{...} by \input{xxx.bbl} and copy the bbl file to the
+  # output directory.  Make sure you run (pdf)latex on the main docuemnt
+  # before running latexpp
+  - 'latexpp.fixes.bib.CopyBblFixes'
+
+  # Expand some macros. Instead of trying to infer the exact expansion that
+  # (pdf)latex would itself perform, you specify here a custom string that the
+  # macro will be expanded to. If the macro has arguments, specify the nature
+  # of the arguments here in the 'argspec:' key (a '*' is an optional *
+  # character, a '[' one optional square-bracket-delimited argument, and a '{'
+  # is a mandatory argument). The argument values are available via the
+  # placeholders %(1)s, %(2)s, etc. Make sure to use single quotes for strings
+  # that contain \ backslashes.
+  - name: 'latexpp.fixes.macro_subst.Fixes'
+    config:
+      macros:
+        # \tr         -->  \operatorname{tr}
+        tr: '\operatorname{tr}'
+        # \ket{\psi}  -->  \lvert{\psi}\rangle
+        ket:
+          argspec: '{'
+          repl: '\lvert{%(1)s}\rangle'
+        # \braket{\psi}{\phi}  -->  \langle{\psi}\vert{\phi}\rangle
+        braket:
+          argspec: '{{'
+          repl: '\langle{%(1)s}\vert{%(2)s}\rangle'
+""".lstrip() # strip leading '\n'
+
+
+class NewLppconfigTemplate(argparse.Action):
+    def __init__(self, **kwargs):
+        super().__init__(help='create a new template lppconfig.yml file and exit',
+                         nargs=0,
+                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # create new YaML template and exit
+        cfgfile = 'lppconfig.yml'
+        if os.path.exists(cfgfile):
+            raise ValueError("The file {} already exists. I won't overwrite it.".format(cfgfile))
+        with open(cfgfile, 'w') as f:
+            f.write(_lppconfig_template)
+        print("Wrote template config file ", cfgfile,
+              ".  Please edit to your liking and then run latexpp.", sep='')
+        sys.exit(0)
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    import argparse
-
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog='latexpp',
+        epilog='See {} for a quick introduction on how to use latexpp.'.format(
+            _LATEXPP_QUICKSTART_DOC_URL
+        ),
+        add_help=False # custom help option
+        )
 
     # this is an optional argument, fname can be specified in lppconfig.yml
     parser.add_argument('fname', metavar='file', nargs='?',
                         help='input file name, master LaTeX document file')
 
-    parser.add_argument('-c', '--lppconfig', action='store', default='lppconfig.yml',
+    parser.add_argument('-c', '--lppconfig', dest='lppconfig',
+                        action='store', default='lppconfig.yml',
                         help='lpp config file (YAML) to use instead of lppconfig.yml')
 
     parser.add_argument('-o', '--output-dir',
@@ -32,22 +122,32 @@ def main(argv=None):
                         default=None,
                         help='output directory (overrides setting from config file)')
 
-    parser.add_argument('--output-fname', dest='output_fname',
+    parser.add_argument('-f', '--output-fname', dest='output_fname',
                         default=None,
                         help='output file name in output directory (overrides '
                         'setting from config file)')
 
     parser.add_argument('-v', '--verbose', dest='verbosity', default=logging.INFO,
                         action='store_const', const=logging.DEBUG,
-                        help='Verbose mode')
+                        help='verbose mode, see what\'s going on in more detail')
+
+    parser.add_argument('--new', action=NewLppconfigTemplate)
+
+    parser.add_argument('--version', action='version', version='%(prog)s {}'.format(version_str))
+    parser.add_argument('--help', action='help', help='show this help message and exit')
 
     args = parser.parse_args(argv)
 
     logging.basicConfig(level=args.verbosity)
 
 
-    with open('lppconfig.yml') as f:
-        lppconfig = yaml.load(f, Loader=yaml.FullLoader)
+    try:
+        with open(args.lppconfig) as f:
+            lppconfig = yaml.load(f, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        logger.error("Can't file config file %s.  See %s for instructions to create a lppconfig file.",
+                     args.lppconfig, _LPPCONFIG_DOC_URL)
+        sys.exit(1)
 
     output_dir = lppconfig.get('output_dir', '_latexpp_output')
     if args.output_dir:
