0df08a699ed99c5a0644083df68f502d61fc1ecf
Philippe Faist <phfaist@gmail.com>
2025-08-02 20:31:24 +0300

    test

--- test/test_fixes_labels.py
+++ test/test_fixes_labels.py
@@ -51,6 +51,48 @@ as~\cref{thm:a94d9b09fe949376,eq:ce3135e517010c8e,eq:unknown}.
         )
 
 
+    def test_cpageref(self):
+
+        lpp = helpers.MockLPP()
+        lpp.install_fix( labels.RenameLabels(use_hash_length=16,
+                                             use_hash_encoding='hex') )
+
+        self.assertEqual(
+            lpp.execute(r"""\documentclass{article}
+\begin{document}
+Here is equation~\eqref{eq:funny-equation}:
+\begin{align}
+  a + b = c\ .
+  \label{eq:funny-equation}
+\end{align}
+And here is the glorious~\cpageref{thm:that-arse-of-a-theorem}.
+\begin{theorem}
+  \label{thm:that-arse-of-a-theorem}
+  Theorem statement here.
+\end{theorem}
+Multiple labels appear here
+as~\cpageref{thm:that-arse-of-a-theorem,eq:funny-equation,eq:unknown}.
+\end{document}
+"""),
+            r"""\documentclass{article}
+\begin{document}
+Here is equation~\eqref{eq:ce3135e517010c8e}:
+\begin{align}
+  a + b = c\ .
+  \label{eq:ce3135e517010c8e}
+\end{align}
+And here is the glorious~\cpageref{thm:a94d9b09fe949376}.
+\begin{theorem}
+  \label{thm:a94d9b09fe949376}
+  Theorem statement here.
+\end{theorem}
+Multiple labels appear here
+as~\cpageref{thm:a94d9b09fe949376,eq:ce3135e517010c8e,eq:unknown}.
+\end{document}
+"""
+        )
+
+
 
 
 
